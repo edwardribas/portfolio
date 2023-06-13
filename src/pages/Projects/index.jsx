a89ac4414950a69d493c91d65094f008b2e4488b
Eduardo Ribas <ribasedu2005@gmail.com>
2023-06-12 22:13:55 -0300

    skeleton loading cards and input design improvement

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -4,13 +4,16 @@ import styles from './styles.module.scss';
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { SkeletonCard } from '../../components/Skeleton';
 
 export default function Projects({ title }){
     const [query, updateQuery] = useState("");
     const [repositories, setRepositories] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     
     const handleGetRepositories = async () => {
         const data = await fetch("https://api.github.com/users/edwardribas/repos").then(e => e.json());
+        setIsLoading(false);
         setRepositories(data.filter(proj => proj.topics.includes('to-portfolio')));
     }
 
@@ -40,7 +43,7 @@ export default function Projects({ title }){
                 </div>
 
                 <div id={styles.projects}>
-                    {repositories.every(proj =>
+                    {!isLoading && repositories.every(proj =>
                         !proj.description.split(' - ')[0].toLowerCase().includes(query.toLowerCase())) && (
                             <p className={styles.not_found_paragraph}>
                                 Sua pesquisa não corresponde a nenhum projeto.
@@ -48,35 +51,41 @@ export default function Projects({ title }){
                         )
                     }
 
-                    {repositories.map((proj, i) => {
-                        const searchQueryIncludeProject = proj.description.split(' - ')[0].toLowerCase().includes(query.toLowerCase());
-
+                    {!isLoading && repositories.map((proj, i) => {
                         const {topics, html_url, homepage, description} = proj;
-                        const projectName = description.split(' - ')[0];
-                        const projectDesc = description.split(' - ')[1]?.split(' #')[0];
-                        const projectImg = description.split(' - ')[1]?.split(' #')[1];
-                        const projectRepo = html_url;
-                        const projectWebsite = homepage;
+                        const searchQueryIncludeProject = proj.description.split(' - ')[0].toLowerCase().includes(query.toLowerCase());
                         const projectStack = topics.filter(e => e.startsWith('tec-'))
                             .map(e => {
                                 const text = e.replace('tec-', '')
                                 return text.slice(0, 1).toUpperCase() + text.slice(1)
                             });
 
+
                         return (
                             searchQueryIncludeProject && (
                                 <ProjectItem
                                     key={i}
-                                    name={projectName}
-                                    description={projectDesc}
-                                    image={projectImg}
-                                    repo={projectRepo}
-                                    preview={projectWebsite}
+                                    name={description.split(' - ')[0]}
+                                    description={description.split(' - ')[1]?.split(' #')[0]}
+                                    image={description.split(' - ')[1]?.split(' #')[1]}
+                                    repo={html_url}
+                                    preview={homepage}
                                     technologies={projectStack}
                                 />
                             )
                         )
                     })}
+                    
+                    {isLoading && (
+                        <>
+                            <SkeletonCard/>
+                            <SkeletonCard/>
+                            <SkeletonCard/>
+                            <SkeletonCard/>
+                            <SkeletonCard/>
+                            <SkeletonCard/>
+                        </>
+                    )}
                 </div>
             </section>
         </Container>
